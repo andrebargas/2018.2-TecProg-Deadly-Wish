78fade5b343b0bd90060f0e444dd4fc95cb3af19
Pedro Daniel <pedrodaniel.unb@gmail.com>
2018-09-03 17:21:16 -0300

    #2 - apply module 2 in headers

--- Deadly-Wish/include/light_attack.h
+++ Deadly-Wish/include/light_attack.h
@@ -22,37 +22,37 @@ using namespace ijengine;
 
 class LightAttack : public Skill {
 public:
-    LightAttack(GameObject *parent, unsigned mage_id, double xp, double yp);
+    LightAttack(GameObject *parent, unsigned hero_id, double x_position, double y_position);
 
     ~LightAttack();
-    bool Active() const;
-    const Rectangle& BoundingBox() const;
-    const list<Rectangle>& HitBoxes() const;
-    pair<double, double> Direction() const;
+    bool is_active() const;
+    const Rectangle& get_bounding_box() const;
+    const list<Rectangle>& get_hit_boxes() const;
+    pair<double, double> get_direction() const;
 
 protected:
-    void UpdateSelf(unsigned now, unsigned last);
-    void DrawSelf(Canvas *canvas, unsigned, unsigned);
-    void UpdateSprite_state();
-    void UpdateTime(unsigned now);
+    void update_self(unsigned now_moment, unsigned last_moment);
+    void draw_self(Canvas *canvas, unsigned, unsigned);
+    void update_sprite_state();
+    void update_time(unsigned now_moment);
     string choose_sprite_path(unsigned player_id);
 
     unsigned character_id;
     typedef enum { MOVING_LEFT, MOVING_RIGHT} State;
     typedef enum { CASTING, MOVING, HITTING } SpriteState;
     typedef enum {PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4} Players;
-    State m_state;
+    State state;
     SpriteState sprite_state;
-    double m_dx, m_dy;
-    int damage;
-    double speed;
+    double x_direction, y_direction;
+    double position_axis_x, position_axis_y;
+    int damage_attack;
+    double speed_attack;
     int frame;
     string sprite_path;
     unsigned start;
     unsigned current_time;
-    shared_ptr<Texture> m_texture;
+    shared_ptr<Texture> texture;
     Rectangle bounding_box;
-
 
 //    bool on_event(const GameEvent& event);
 };
