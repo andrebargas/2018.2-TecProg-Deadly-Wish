1d9db81f9e3729f8d813145884748116c2cbd73c
andrebargas <andrebargas@gmail.com>
2018-09-03 17:22:46 -0300

    Apply mod 2 to Spear class
    Co-authored-by:jpmota2208<jpmota2208@gmail.com>

--- Deadly-Wish/include/spear.h
+++ Deadly-Wish/include/spear.h
@@ -21,15 +21,16 @@ using namespace std;
 using namespace ijengine;
 
 class Spear : public Skill {
+
 public:
-    Spear(GameObject *parent, unsigned soldier_id, double xp, double yp,
-        double dx, double dy);
+    Spear(GameObject *parent, unsigned soldier_id, double x_position, double y_position,
+        double x_direction, double y_direction);
 
     ~Spear();
-    bool active() const;
-    const Rectangle& bounding_box() const;
-    const list<Rectangle>& hit_boxes() const;
-    pair<double, double> direction() const;
+    bool is_active() const;
+    const Rectangle& get_bounding_box() const;
+    const list<Rectangle>& get_hit_boxes() const;
+    pair<double, double> get_direction() const;
 
 protected:
     void update_self(unsigned now, unsigned last);
@@ -38,21 +39,21 @@ protected:
     void update_time(unsigned now);
     string choose_sprite_path(unsigned player_id);
 
-    unsigned m_character_id;
+    unsigned character_id;
     typedef enum { MOVING_LEFT, MOVING_RIGHT} State;
     typedef enum { THROW, MOVING, FALLING, HITTING } SpriteState;
     typedef enum {PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4} Players;
-    State m_state;
-    SpriteState m_sprite_state;
-    double m_dx, m_dy;
-    int m_damage;
-    double m_speed;
-    int m_frame;
-    string m_sprite_path;
-    unsigned m_start;
-    unsigned m_current_time;
-    shared_ptr<Texture> m_texture;
-    Rectangle m_bounding_box;
+    State state;
+    SpriteState prite_state;
+    double axis_x_direction, axis_y_direction;
+    int damage;
+    double speed;
+    int frame;
+    string sprite_path;
+    unsigned start;
+    unsigned current_time;
+    shared_ptr<Texture> texture;
+    Rectangle bounding_box;
 
 
 //    bool on_event(const GameEvent& event);
