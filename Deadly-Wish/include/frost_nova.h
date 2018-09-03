9c69ac46298ce715ad308ef788d0e8fd008d32f7
Pedro Daniel <pedrodaniel.unb@gmail.com>
2018-09-03 17:21:16 -0300

    #2 - apply module 2 in headers

--- Deadly-Wish/include/frost_nova.h
+++ Deadly-Wish/include/frost_nova.h
@@ -22,40 +22,41 @@ using namespace ijengine;
 
 class FrostNova : public Skill {
 public:
-    FrostNova(GameObject *parent, unsigned mage_id, double xp, double yp,
-        double dx, double dy);
+    FrostNova(GameObject *parent, unsigned hero_id, double x_position, double y_position,
+        double x_direction, double y_direction);
 
 
 
     ~FrostNova();
-    bool active() const;
-    const Rectangle& bounding_box() const;
-    const list<Rectangle>& hit_boxes() const;
-    pair<double, double> direction() const;
-    void on_collision(const Collidable *who, const Rectangle& where, unsigned now, unsigned last);
+    bool is_active() const;
+    const Rectangle& get_bounding_box() const;
+    const list<Rectangle>& get_hit_boxes() const;
+    pair<double, double> get_direction() const;
+    void on_collision(const Collidable *who, const Rectangle& where, unsigned now_moment, unsigned last_moment);
 
 protected:
-    void update_self(unsigned now, unsigned last);
+    void update_self(unsigned now_moment, unsigned last_moment);
     void draw_self(Canvas *canvas, unsigned, unsigned);
     void update_sprite_state();
-    void update_time(unsigned now);
+    void update_time(unsigned now_moment);
     string choose_sprite_path(unsigned player_id);
 
-    unsigned m_character_id;
+    unsigned character_id;
     typedef enum { MOVING_LEFT, MOVING_RIGHT} State;
     typedef enum { CASTING, MOVING, HITTING } SpriteState;
     typedef enum {PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4} Players;
-    State m_state;
-    SpriteState m_sprite_state;
-    double m_dx, m_dy;
-    int m_damage;
-    double m_speed;
-    int m_frame;
-    string m_sprite_path;
-    unsigned m_start;
-    unsigned m_current_time;
-    shared_ptr<Texture> m_texture;
-    Rectangle m_bounding_box;
+    State state;
+    SpriteState sprite_state;
+    double skill_x_direction, skill_y_direction;
+    double x_position_frost_nova, y_position_frost_nova;
+    int new_skill_damage;
+    double speed_attack;
+    int frame;
+    string sprite_path;
+    unsigned start;
+    unsigned current_time;
+    shared_ptr<Texture> texture;
+    Rectangle bounding_box;
 
 
 //    bool on_event(const GameEvent& event);
--- Deadly-Wish/include/light_attack.h
+++ Deadly-Wish/include/light_attack.h
@@ -22,36 +22,37 @@ using namespace ijengine;
 
 class LightAttack : public Skill {
 public:
-    LightAttack(GameObject *parent, unsigned mage_id, double xp, double yp);
+    LightAttack(GameObject *parent, unsigned hero_id, double x_position, double y_position);
 
     ~LightAttack();
-    bool active() const;
-    const Rectangle& bounding_box() const;
-    const list<Rectangle>& hit_boxes() const;
-    pair<double, double> direction() const;
+    bool is_active() const;
+    const Rectangle& get_bounding_box() const;
+    const list<Rectangle>& get_hit_boxes() const;
+    pair<double, double> get_direction() const;
 
 protected:
-    void update_self(unsigned now, unsigned last);
+    void update_self(unsigned now_moment, unsigned last_moment);
     void draw_self(Canvas *canvas, unsigned, unsigned);
     void update_sprite_state();
-    void update_time(unsigned now);
+    void update_time(unsigned now_moment);
     string choose_sprite_path(unsigned player_id);
 
-    unsigned m_character_id;
+    unsigned character_id;
     typedef enum { MOVING_LEFT, MOVING_RIGHT} State;
     typedef enum { CASTING, MOVING, HITTING } SpriteState;
     typedef enum {PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4} Players;
-    State m_state;
-    SpriteState m_sprite_state;
-    double m_dx, m_dy;
-    int m_damage;
-    double m_speed;
-    int m_frame;
-    string m_sprite_path;
-    unsigned m_start;
-    unsigned m_current_time;
-    shared_ptr<Texture> m_texture;
-    Rectangle m_bounding_box;
+    State state;
+    SpriteState sprite_state;
+    double x_direction, y_direction;
+    double position_axis_x, position_axis_y;
+    int damage_attack;
+    double speed_attack;
+    int frame;
+    string sprite_path;
+    unsigned start;
+    unsigned current_time;
+    shared_ptr<Texture> texture;
+    Rectangle bounding_box;
 
 
 //    bool on_event(const GameEvent& event);
