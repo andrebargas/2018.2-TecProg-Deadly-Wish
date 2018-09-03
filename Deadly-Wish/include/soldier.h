ca906f19414db75f77ce8d907f8ca1b66b118a16
andrebargas <andrebargas@gmail.com>
2018-09-03 12:04:11 -0300

    Adding module 2 changes to Soldier class
    Co-authored-by: Fabiana<fabianalv.ribas@gmail.com>

--- Deadly-Wish/include/soldier.h
+++ Deadly-Wish/include/soldier.h
@@ -7,14 +7,14 @@ using namespace ijengine;
 
 class Soldier : public Character {
 public:
-    Soldier(vector<string> sprite_paths, unsigned id,
-     double x, double y, int character_code);
-    
+    Soldier(vector<string> sprite_paths, unsigned new_character_id,
+     double position_axis_x, double position_axis_y, int character_code);
+
 protected:
-    void DoHeavyAttack();
-    void DoLightAttack();
-    void DoDefense();
-    void DoSpecial();
+    void do_heavy_attack();
+    void do_light_attack();
+    void do_defense();
+    void do_special();
 };
 
 #endif
--- Deadly-Wish/src/soldier.cpp
+++ Deadly-Wish/src/soldier.cpp
@@ -3,17 +3,18 @@
 #include "light_attack.h"
 #include "ije02_game.h"
 
-#define MAX_LIFE 120
+#define MAX_LIFE 150
 #define RECHARGE_SPECIAL 5000
 #define RECHARGE_HEAVY_ATTACK 2000
 #define RECHARGE_LIGHT_ATTACK 300
 #define RECHARGE_DEFENSE 300
-#define LIGHT_MOVIMENT 15 
-#define HEAVY_MOVIMENT 20 
+#define LIGHT_MOVIMENT 15
+#define HEAVY_MOVIMENT 20
+#define VARIATION_DIRECTION_HEAVY_ATTACK 1.0
 
-Soldier::Soldier(vector<string> sprite_paths, unsigned id,
-    double x, double y, int character_code)
-    : Character(sprite_paths, id, x, y, MAX_LIFE, character_code) {
+Soldier::Soldier(vector<string> sprite_paths, unsigned new_character_id,
+                 double position_axis_x, double position_axis_y, int character_code)
+: Character(sprite_paths, new_character_id, position_axis_x, position_axis_y, MAX_LIFE, character_code) {
 
     special_cooldown = RECHARGE_SPECIAL;
     heavy_attack_cooldown = RECHARGE_HEAVY_ATTACK;
@@ -26,56 +27,56 @@ Soldier::Soldier(vector<string> sprite_paths, unsigned id,
     active = true;
 }
 
-void Soldier::DoHeavyAttack() {
+void Soldier::do_heavy_attack() {
 
     audio::play_sound_effect("res/sound/fx/soldier_heavy.ogg", EFFECTS_VOLUME, 0);
     auto p = parent();
     printf("p = %p\n", (void *) p);
 
-    double spear_dx = 0.0;
-    double spear_x_pos = 0.0;
+    double spear_direction_x = 0.0;
+    double spear_x_position = 0.0;
 
-    if(m_moving_state == MOVING_RIGHT) {
-        spear_dx = 1.0;
-        spear_x_pos = x() + HEAVY_MOVIMENT;
+    if(moving_state == MOVING_RIGHT) {
+        spear_direction_x = VARIATION_DIRECTION_HEAVY_ATTACK;
+        spear_x_position = x() + HEAVY_MOVIMENT;
     }
     else{
-        spear_dx = -1.0;
-        spear_x_pos = x() - HEAVY_MOVIMENT;
+        spear_direction_x = -(VARIATION_DIRECTION_HEAVY_ATTACK);
+        spear_x_position = x() - HEAVY_MOVIMENT;
     }
 
-    p->add_child(new Spear(p, id(), spear_x_pos, y(), spear_dx, 0.0));
-    ChangeCharacterState(HEAVY_ATTACK_STATE);
+    p->add_child(new Spear(p, get_id(), spear_x_position, y(), spear_dx, 0.0));
+    change_character_state(HEAVY_ATTACK_STATE);
 }
 
-void Soldier::DoLightAttack() {
+void Soldier::do_light_attack() {
 
     audio::play_sound_effect("res/sound/fx/soldier_light.ogg", EFFECTS_VOLUME, 0);
     auto p = parent();
     printf("p = %p\n", (void *) p);
 
-    double light_attack_x_pos = 0.0;
-    
-    if(m_moving_state == MOVING_RIGHT) {
-        light_attack_x_pos = x() + LIGHT_MOVIMENT;
+    double light_attack_x_position = 0.0;
+
+    if(moving_state == MOVING_RIGHT) {
+        light_attack_x_position = x() + LIGHT_MOVIMENT;
     }
     else{
-        light_attack_x_pos = x() - LIGHT_MOVIMENT;
+        light_attack_x_position = x() - LIGHT_MOVIMENT;
     }
 
-    p->add_child(new DoLightAttack(p, id(), light_attack_x_pos, y()));
+    p->add_child(new LightAttack(p, get_id(), light_attack_x_position, y()));
 
-    ChangeCharacterState(LIGHT_ATTACK_STATE);
+    change_character_state(LIGHT_ATTACK_STATE);
 }
 
-void Soldier::DoDefense() {
+void Soldier::do_defense() {
 
     audio::play_sound_effect("res/sound/fx/soldier_block.ogg", EFFECTS_VOLUME, 0);
-    ChangeCharacterState(DEFENSE_STATE);
+    change_character_state(DEFENSE_STATE);
 }
 
-void Soldier::DoSpecial() {
+void Soldier::do_special() {
 
     audio::play_sound_effect("res/sound/fx/soldier_ultimate.ogg", EFFECTS_VOLUME, 0);
-    ChangeCharacterState(SPECIAL_STATE);
+    change_character_state(SPECIAL_STATE);
 }
