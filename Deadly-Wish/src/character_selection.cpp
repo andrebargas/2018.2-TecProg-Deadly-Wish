93cbdfc6e9193229ff1153647c8971d175a8a1d3
Gustavo Duarte Moreira <gustavoduartemoreira@gmail.com>
2018-09-03 21:26:00 -0300

    Appy mod_2 in character_selection.h, character_selection.cpp

--- Deadly-Wish/src/character_selection.cpp
+++ Deadly-Wish/src/character_selection.cpp
@@ -7,16 +7,21 @@
 
 #include <cmath>
 
+#define CHARACTER_WIDTH 8
+#define CHARACTER_HEIGHT 9
+
 using namespace std;
 
-CharacterSelection::CharacterSelection(const string sprite_path, int base_x, int base_y, int player_id)
-    : m_base_x(base_x), m_base_y(base_y), m_player_id(player_id), m_frame(player_id), m_start(-1), m_current_selection(KNIGHT)
-{
+CharacterSelection::CharacterSelection(const string sprite_path, int base_x, 
+                                       int base_y, int player_id)
+    : axis_base_x(base_x), axis_base_y(base_y), m_player_id(player_id), frame(player_id), 
+      start(-1), current_selection(KNIGHT) {
     m_texture = resources::get_texture(sprite_path);
-    m_w = 8;
-    m_h = 9;
 
-    m_chosen = false;
+    width = CHARACTER_WIDTH;
+    height = CHARACTER_HEIGHT;
+
+    chosen = false;
 
     event::register_listener(this);
     update_position();
@@ -26,91 +31,79 @@ CharacterSelection::~CharacterSelection() {
     event::unregister_listener(this);
 }
 
-void
-CharacterSelection::update_self(unsigned now, unsigned last)
-{
-    if (m_start == -1)
-        m_start = now;
+void CharacterSelection::update_self(unsigned now, unsigned last) {
+    if (start == -1)
+        start = now;
 
-    if (now - m_start > 100)
-    {
-        m_start += 100;
+    if (now - start > 100) {
+        start += 100;
     }
 }
 
 const int DIFF_X = 43;
 const int DIFF_Y = 42;
 
-// BASE X = 135
-// BASE Y = 73
-void
-CharacterSelection::update_position()
-{
-    switch(m_current_selection) {
+void CharacterSelection::update_position() {
+    switch(current_selection) {
         case KNIGHT:
-            set_x(m_base_x);
-            set_y(m_base_y);
-            break;
+            set_x(axis_base_x);
+            set_y(axis_base_y);
+        break;
 
         case SOLDIER:
-            set_x(m_base_x + DIFF_X);
-            set_y(m_base_y);
-            break;
+            set_x(axis_base_x + DIFF_X);
+            set_y(axis_base_y);
+        break;
 
         case MAGE:
-            set_x(m_base_x);
-            set_y(m_base_y + DIFF_Y);
-            break;
+            set_x(axis_base_x);
+            set_y(axis_base_y + DIFF_Y);
+        break;
 
         case INFILTRATOR:
-            set_x(m_base_x + DIFF_X);
-            set_y(m_base_y + DIFF_Y);
-            break;
+            set_x(axis_base_x + DIFF_X);
+            set_y(axis_base_y + DIFF_Y);
+        break;
 
         default:
             printf("Argumento inválido no update_position() do character_selection.cpp\n");
-            break;
+        break;
     }
 }
 
-void
-CharacterSelection::draw_self(Canvas *canvas, unsigned, unsigned)
-{
-    Rectangle rect {0.0, (m_texture->h() / (double) 4) * m_frame, (double) m_w, (double) m_h};
+void CharacterSelection::draw_self(Canvas *canvas, unsigned, unsigned) {
+    Rectangle rect {0.0, (m_texture->h() / (double) 4) * frame, (double) width, (double) height};
     canvas->draw(m_texture.get(), rect, x(), y());
 }
 
-bool
-CharacterSelection::on_event(const GameEvent &event)
-{
-
+bool CharacterSelection::on_event(const GameEvent &event) {
     if(((event.id() == game_event::MOVEMENT_P1 && m_player_id == 0) ||
        (event.id() == game_event::MOVEMENT_P2 && m_player_id == 1) ||
        (event.id() == game_event::MOVEMENT_P3 && m_player_id == 2) ||
        (event.id() == game_event::MOVEMENT_P4 && m_player_id == 3)) and 
-       (m_chosen == false)) {
+       (chosen == false)) {
         string axis = event.get_property<string>("axis");
         int value = event.get_property<int>("value");
 
         if(axis == "X") {
             if(value > 0) {
-                m_current_selection = (m_current_selection + 1) % 4;
+                current_selection = (current_selection + 1) % 4;
             }
             else if(value < 0) {
-                m_current_selection = (m_current_selection - 1) % 4;
+                current_selection = (current_selection - 1) % 4;
             }
         }
         else if(axis == "Y") {
             if(value > 0) {
-                m_current_selection = (m_current_selection - 2) % 4;
+                current_selection = (current_selection - 2) % 4;
             }
             else if(value < 0) {
-                m_current_selection = (m_current_selection + 2) % 4;
+                current_selection = (current_selection + 2) % 4;
             }
         }
         
-        if(m_current_selection < 0) {
-            m_current_selection += 4;
+        if(current_selection < 0) {
+            current_selection += 4;
         }
         update_position();
         return true;
@@ -119,13 +112,13 @@ CharacterSelection::on_event(const GameEvent &event)
             (event.id() == game_event::LIGHT_ATTACK_P2 && m_player_id == 1) ||
             (event.id() == game_event::LIGHT_ATTACK_P3 && m_player_id == 2) ||
             (event.id() == game_event::LIGHT_ATTACK_P4 && m_player_id == 3)) {
-        m_chosen = true;
+        chosen = true;
     }
     else if((event.id() == game_event::DEFENSE_P1 && m_player_id == 0) ||
             (event.id() == game_event::DEFENSE_P2 && m_player_id == 1) ||
             (event.id() == game_event::DEFENSE_P3 && m_player_id == 2) ||
             (event.id() == game_event::DEFENSE_P4 && m_player_id == 3)) {
-        m_chosen = false;
+        chosen = false;
    }
     return false;
 }
