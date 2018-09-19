25d97fd20ba9eb87f4531a39b873b7f4693a6561
Pedro Daniel <pedrodaniel.unb@gmail.com>
2018-09-18 23:30:39 -0300

    #27 - Updating comentaries in Mage

--- Deadly-Wish/src/mage.cpp
+++ Deadly-Wish/src/mage.cpp
@@ -1,3 +1,7 @@
+/** \file mage.cpp
+  * \brief Este é o arquivo da classe Mage que é filha da classe Character.
+  */
+
 #include "mage.h"
 #include "fireball.h"
 #include "frost_nova.h"
@@ -22,16 +26,15 @@
 using std::vector;
 using std::string;
 
-
-//! Inicializando Método Construtor
-/*!
-\param sprite_paths - desenha personagem
-\param id - identificador do personagem
-\param x - posição no eixo x
-\param y - posição no eixo y
-\param character_code - codigo do personagem
-*/
-
+/** \fn Mage(vector<string> sprite_paths, unsigned id, double x, double y, int character_code)
+  * \public
+  * \brief Método Construtor
+  * \param sprite_paths - desenha personagem
+  * \param id - identificador do personagem
+  * \param x - posição no eixo x
+  * \param y - posição no eixo y
+  * \param character_code - codigo do personagem
+  */
 Mage::Mage(vector<string> sprite_paths, unsigned id, double x, double y, int character_code)
     : Character(sprite_paths, id, x, y, MAX_LIFE, character_code)
 {
@@ -55,20 +58,23 @@ Mage::Mage(vector<string> sprite_paths, unsigned id, double x, double y, int cha
     character_active = true;
 }
 
-//! Método void que retorna ataque pesado, poder do ataque: 2000
+/** \fn do_heavy_attack()
+  * \protected
+  * \brief Método que executa um ataque pesado
+  */
 void
 Mage::do_heavy_attack()
 {
-    //! Som do heavy_atack
+    // executa o som do heavy_atack
     audio::play_sound_effect("res/sound/fx/mago_heavy.ogg", EFFECTS_VOLUME, 0);
     auto p = parent();
     printf("p = %p\n", (void *) p);
 
-    //! Direção e posição a fireball (eixo x)
+    // Direção e posição a fireball (eixo x)
     double fireball_dx = 0.0;
     double fireball_x_pos = 0.0;
 
-    //! Se o personagem estiver para direita
+    // Se o personagem estiver se movendo para a direita
     if(character_moving_state == MOVING_RIGHT) {
         fireball_dx = 1.0;
         fireball_x_pos = x() + 20;
@@ -80,14 +86,17 @@ Mage::do_heavy_attack()
 
     p->add_child(new Fireball(p, get_id(), fireball_x_pos, y(), fireball_dx, 0.0));
 
-    //! Muda o estado do personagem
+    // Muda o estado do personagem
     change_character_state(HEAVY_ATTACK_STATE);
 }
 
-//! Método void que retorna ataque leve, poder do ataque: 300
+/** \fn do_light_attack()
+  * \protected
+  * \brief Método que executa um ataque leve
+  */
 void
 Mage::do_light_attack() {
-    //! Som do light_atack
+    // executa o som do light_atack
     audio::play_sound_effect("res/sound/fx/mago_light.ogg", EFFECTS_VOLUME, 0);
     auto p = parent();
     printf("p = %p\n", (void *) p);
@@ -95,7 +104,7 @@ Mage::do_light_attack() {
     // posicao do ataque no eixo x
     double light_attack_x_pos = 0.0;
 
-    // se o personagem estiver para direita 
+    // se o personagem estiver se movendo para a direita 
     if(character_moving_state == MOVING_RIGHT) {
         light_attack_x_pos = x() + 15;
     }
@@ -105,23 +114,29 @@ Mage::do_light_attack() {
 
     p->add_child(new LightAttack(p, get_id(), light_attack_x_pos, y()));
 
-    //! Muda o estado do personagem
+    // Muda o estado do personagem
     change_character_state(LIGHT_ATTACK_STATE);
 }
 
-//! Método void que retorna a defesa, poder da defesa: 300
+/** \fn do_defense()
+  * \protected
+  * \brief Método que executa uma defesa
+  */
 void
 Mage::do_defense() {
-    //som da defesa
+    // executa o som de defesa
     audio::play_sound_effect("res/sound/fx/mago_block.ogg", EFFECTS_VOLUME, 0);
     // muda o estado do personagem
     change_character_state(DEFENSE_STATE);
 }
 
-// Método void que retorna ataque especial, poder do ataque: 5000
+/** \fn do_special()
+  * \protected
+  * \brief Método que executa um ataque especial
+  */
 void
 Mage::do_special() {
-    // som do poder especial
+    // executa o som do poder especial
     audio::play_sound_effect("res/sound/fx/mago_ultimate.ogg", EFFECTS_VOLUME, 0);
     auto p = parent();
     printf("p = %p\n", (void *) p);
