7c8dcb95c498008bee8c8636c93f12fc06779b0f
Gustavo Duarte Moreira <gustavoduartemoreira@gmail.com>
2018-09-20 20:59:13 -0300

    #31 Apply module 03 in skill.cpp skill.h

--- Deadly-Wish/include/skill.h
+++ Deadly-Wish/include/skill.h
@@ -1,6 +1,11 @@
+/** \file skill.h
+  * \brief Este é o arquivo da classe de habilidade dos personagens no jogo, com herança de 
+  * GameObject e Collidable da endige do jogo.
+  */
 #ifndef SKILL_H
 #define SKILL_H
 
+//!
 #include <ijengine/game_object.h>
 #include <ijengine/rectangle.h>
 #include <ijengine/canvas.h>
@@ -9,29 +14,29 @@
 
 using namespace ijengine;
 
-class Skill : public GameObject, public Collidable  {
-public:
+class Skill : public GameObject, public Collidable
+{
+  public:
     Skill(GameObject *parent, double x_posiotion, double y_posiotion, int damage,
           int character_id);
     ~Skill();
 
     unsigned get_character_id() const { return skill_character_id; }
     int get_damage() const { return skill_damage; }
-    int get_collided() const {return skill_collided;}
+    int get_collided() const { return skill_collided; }
 
     //metodos virtual Colidable
     virtual bool active() const = 0;
-    virtual const Rectangle& bounding_box() const = 0;
-    virtual const list<Rectangle>& hit_boxes() const = 0;
+    virtual const Rectangle &bounding_box() const = 0;
+    virtual const list<Rectangle> &hit_boxes() const = 0;
     virtual pair<double, double> direction() const = 0;
-    virtual void on_collision(const Collidable *who, const Rectangle& where, unsigned now, unsigned last);
+    virtual void on_collision(const Collidable *who, const Rectangle &where, unsigned now, unsigned last);
 
-protected:
+  protected:
     unsigned skill_character_id;
     int skill_damage;
     double skill_speed;
     int skill_collided;
-
 };
 
 #endif
