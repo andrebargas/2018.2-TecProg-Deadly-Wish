e83fac124efafca6475d3188c3c207ec9181b5f4
Gustavo Duarte Moreira <gustavoduartemoreira@gmail.com>
2018-09-20 23:31:12 -0300

    #31 Apply module 03 in skill.h light_attack.h light_attack.cpp

--- Deadly-Wish/include/skill.h
+++ Deadly-Wish/include/skill.h
@@ -1,11 +1,11 @@
 /** \file skill.h
-  * \brief Este é o arquivo da classe de habilidade dos personagens no jogo, com herança de 
-  * GameObject e Collidable da endige do jogo.
+  * \brief Este é o arquivo header da classe Skill, de habilidade dos personagens no jogo, 
+  * com herança de GameObject e Collidable da endige do jogo.
   */
 #ifndef SKILL_H
 #define SKILL_H
 
-//!
+//! Inclui os arquivos da engine do jogo
 #include <ijengine/game_object.h>
 #include <ijengine/rectangle.h>
 #include <ijengine/canvas.h>
@@ -14,18 +14,49 @@
 
 using namespace ijengine;
 
+/** \class Skill skill.h "include/skill.h"
+ *  \brief Classe que herda de GameObject e Collidable
+ *  que calcula o dano recebido pelos personagens.
+ * \breif Método Construtor Skill
+ * \param *parent Ponteiro para o GameObject
+ * \param x_posiotion double posição no eixo X
+ * \param y_posiotion double posição no eixo y
+ * \param damage int dano da habilidade
+ * \param character_id int identificador do personagem
+ */
 class Skill : public GameObject, public Collidable
 {
   public:
+    /** \fn Skill(GameObject *parent, double x_posiotion, double y_posiotion, int damage,
+        int character_id)
+     * \public
+     * \brief Método construtor
+     */
     Skill(GameObject *parent, double x_posiotion, double y_posiotion, int damage,
           int character_id);
+
+    /** \fn ~Skill()
+      * \public
+      * \brief Método destrutor
+      */
     ~Skill();
 
-    unsigned get_character_id() const { return skill_character_id; }
-    int get_damage() const { return skill_damage; }
+    //! Método que retorna a habilidade de um personagem
+    unsigned get_character_id() const
+    {
+        return skill_character_id;
+    }
+
+    //! Método que retorna a skill_damage
+    int get_damage() const
+    {
+        return skill_damage;
+    }
+
+    //! Método que retorna a colisão
     int get_collided() const { return skill_collided; }
 
-    //metodos virtual Colidable
+    //! Métodos virtuais de colisão
     virtual bool active() const = 0;
     virtual const Rectangle &bounding_box() const = 0;
     virtual const list<Rectangle> &hit_boxes() const = 0;
@@ -33,9 +64,13 @@ class Skill : public GameObject, public Collidable
     virtual void on_collision(const Collidable *who, const Rectangle &where, unsigned now, unsigned last);
 
   protected:
+    //! Variável que indica o character_id skill
     unsigned skill_character_id;
+    //! Variável que indica o dano do skill
     int skill_damage;
+    //! Variável que indica a velocidade do skill
     double skill_speed;
+    //! Variável que indica a colisão do skill
     int skill_collided;
 };
 
